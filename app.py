adc7641ae876bd06ddcac98d93efa490470b5552
indavrs-wq <ind.av.rs@gmail.com>
2026-05-19 19:49:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Python API Working"
+
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    data = request.json
+
+    name = data.get("name", "")
+
+    result = {
+        "success": True,
+        "message": f"Hello {name} from Python"
+    }
+
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
